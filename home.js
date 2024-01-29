301b9e5e4c3b1f7345fc4938af6cdf79b5f693d3
Giada Monni <gm.kyraraebrian@gmail.com>
2024-01-29 01:41:21 +0100

    iniziato sito

---
+++ home.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const menuIcon = document.getElementById("menuIcon");
+    const dropdown = document.querySelector(".dropdown");
+  
+    menuIcon.addEventListener("click", function() {
+        menuIcon.classList.toggle("active");
+    });
+  });
+
+
+var swiper = new Swiper(".swiper.mySwiper", {
+    spaceBetween: 30,
+    effect: "fade",
+    loop: true,
+    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    autoplay: {
+        delay: 2000,
+      },
+  });
+
+
